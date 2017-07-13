0b0eced1407fe38d976b23109d8ad2f3b52a5cef
YuhanLee <yuhan1995@gmail.com>
2017-07-13 18:41:33 -0400

    Update SportsStore app on July 13th

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+@Component ({
+selector: "app",
+template: "<store></store>"
 })
-export class AppComponent {
-  title = 'app';
-}
+
+export class AppComponent{}
